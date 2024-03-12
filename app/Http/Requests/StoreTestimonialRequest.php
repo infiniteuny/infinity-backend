5747d693970da57100b49e584e71714d55b7e920
Azis Rosyid <azisrosyid321@gmail.com>
2024-03-12 17:18:21 +0700

    feat(requests): Add new requests

---
+++ app/Http/Requests/StoreTestimonialRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTestimonialRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'position' => 'required|regex:/^[a-zA-Z.0-9.\s]+$/|max:255',
+            'photo' => 'required|image|mimes:jpg,png,jpeg|max:2048',
+            'content' => 'required|string',
+        ];
+    }
+}
