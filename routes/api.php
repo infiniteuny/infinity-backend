b95857bb8e8f41ae87bd15242d9ac7e59e715fed
Gib-byte <gibranabyan082@gmail.com>
2024-04-30 13:19:03 +0700

    feat(api): Add api route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AchievementController;
+use App\Http\Controllers\Api\v1\CompetitionController;
+use App\Http\Controllers\Api\v1\CompetitionOrganizerTypeController;
+use App\Http\Controllers\Api\v1\CompetitionOutputController;
+use App\Http\Controllers\Api\v1\CompetitionRankController;
+use App\Http\Controllers\Api\v1\CompetitionScaleController;
+use App\Http\Controllers\Api\v1\CompetitionTeamTypeController;
+use App\Http\Controllers\Api\v1\CompetitionTimeRangeController;
+use App\Http\Controllers\Api\v1\ConfigController;
+use App\Http\Controllers\Api\v1\CoreTeamController;
+use App\Http\Controllers\Api\v1\CoreTeamDivisionController;
+use App\Http\Controllers\Api\v1\CoreTeamMemberController;
+use App\Http\Controllers\Api\v1\DegreeController;
+use App\Http\Controllers\Api\v1\FundApplicationController;
+use App\Http\Controllers\Api\v1\MajorController;
+use App\Http\Controllers\Api\v1\PostController;
+use App\Http\Controllers\Api\v1\ProjectGalleryController;
+use App\Http\Controllers\Api\v1\TeamController;
+use App\Http\Controllers\Api\v1\TeamMemberController;
+use App\Http\Controllers\Api\v1\TestimonialController;
+use App\Http\Controllers\Api\v1\UserController;
 use App\Http\Controllers\FacultyController;
 use App\Http\Controllers\MemberController;
+use App\Models\CompetitionOrganizerType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,3 +47,27 @@ Route::get('/faculties/{faculty}/program-studies', [FacultyController::class, 'p
 Route::get('/members', [MemberController::class, 'membersList'])->name('members.list');
 
 Route::middleware('auth:sanctum')->post('/uid', [MemberController::class, 'uid'])->name('members.uid');
+
+// NEW DASHBOARD
+Route::resource('achievement', AchievementController::class);
+Route::resource('competition', CompetitionController::class);
+Route::resource('competition-organizer-type', CompetitionOrganizerTypeController::class);
+Route::resource('competition-output', CompetitionOutputController::class);
+Route::resource('competition-rank', CompetitionRankController::class);
+Route::resource('competition-scale', CompetitionScaleController::class);
+Route::resource('competition-team-type', CompetitionTeamTypeController::class);
+Route::resource('competition-time-range', CompetitionTimeRangeController::class);
+Route::resource('config', ConfigController::class);
+Route::resource('core-team', CoreTeamController::class);
+Route::resource('core-team-division', CoreTeamDivisionController::class);
+Route::resource('core-team-member', CoreTeamMemberController::class);
+Route::resource('degree', DegreeController::class);
+Route::resource('faculty', FacultyController::class);
+Route::resource('fund-application', FundApplicationController::class);
+Route::resource('major', MajorController::class);
+Route::resource('post', PostController::class);
+Route::resource('project-gallery', ProjectGalleryController::class);
+Route::resource('team', TeamController::class);
+Route::resource('team-member', TeamMemberController::class);
+Route::resource('testimonial', TestimonialController::class);
+Route::resource('user', UserController::class);
