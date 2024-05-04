db6635578f60ee982a48abc13fcfcb7bb940f983
Gib-byte <gibranabyan082@gmail.com>
2024-05-04 20:34:41 +0700

    [WIP] feat(crud): Add degree repository
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Http/Controllers/Api/v1/DegreeController.php
+++ app/Http/Controllers/Api/v1/DegreeController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Degree\StoreDegreeRequest;
 use App\Http\Requests\Degree\UpdateDegreeRequest;
 use App\Models\Degree;
+use App\Repository\DegreeRepository;
+use Illuminate\Http\Request;
 
 class DegreeController extends Controller
 {
@@ -18,9 +20,9 @@ class DegreeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index(Request $request, DegreeRepository $degreeRepository)
     {
-        return $this->degreeRepository->index($request);
+        return $degreeRepository->index($request);
     }
 
     /**
