206d826f66d00e3d7795c47ac0e3cf74ba4545b6
GoSter223 <alifakbarhilmyauliya2003@gmail.com>
2024-05-08 22:19:17 +0700

    feat(controller): Add team CRUD
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Http/Controllers/Api/V1/TeamController.php
+++ app/Http/Controllers/Api/V1/TeamController.php
@@ -6,51 +6,81 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Team\StoreTeamRequest;
 use App\Http\Requests\Team\UpdateTeamRequest;
 use App\Models\Team;
+use App\Utils\ResponseFormatter;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class TeamController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(Team::class, 'Team');
+        // $this->authorizeResource(Team::class, 'Team');
     }
 
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request): JsonResponse
     {
-        //
+        $teams = QueryBuilder::for(Team::class)
+            ->allowedFilters([
+                'leader_id',
+                'name',
+                'is_personal',
+            ])
+            ->defaultSorts([
+                '-created_at',
+                'id',
+            ])
+            ->allowedSorts([
+                'id',
+                'leader_id',
+                'name',
+                'is_personal',
+                'created_at',
+                'updated_at',
+            ])
+            ->paginate($request->query('per_page', 10));
+
+        return ResponseFormatter::collection('teams', $teams);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreTeamRequest $request)
+    public function store(StoreTeamRequest $request): JsonResponse
     {
-        //
+        $team = Team::create($request->validated());
+
+        return ResponseFormatter::singleton('team', $team, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Team $team)
+    public function show(Team $team): JsonResponse
     {
-        //
+        return ResponseFormatter::singleton('team', $team);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTeamRequest $request, Team $team)
+    public function update(UpdateTeamRequest $request, Team $team): JsonResponse
     {
-        //
+        $team->update($request->validated());
+
+        return ResponseFormatter::singleton('team', $team);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Team $team)
+    public function destroy(Team $team): JsonResponse
     {
-        //
+        $team->delete();
+
+        return ResponseFormatter::singleton('team', $team);
     }
 }
