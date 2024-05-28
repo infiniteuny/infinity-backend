1ed118d918a566f9315f803b5f4dde83ba2a6b2d
GoSter223 <alifakbarhilmyauliya2003@gmail.com>
2024-05-28 20:14:33 +0700

    feat(controller): Add `FundApplication` CRUD
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Http/Controllers/Api/V1/FundApplicationController.php
+++ app/Http/Controllers/Api/V1/FundApplicationController.php
@@ -6,20 +6,56 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\FundApplication\StoreFundApplicationRequest;
 use App\Http\Requests\FundApplication\UpdateFundApplicationRequest;
 use App\Models\FundApplication;
+use App\Utils\ResponseFormatter;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class FundApplicationController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(FundApplication::class, 'fundApplication');
+        // $this->authorizeResource(FundApplication::class, 'fund_application');
     }
 
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request): JsonResponse
     {
-        //
+        $fundApplications = QueryBuilder::for(FundApplication::class)
+            ->allowedFilters([
+                'team_id',
+                'competition_id',
+                'competition_team_type_id',
+                'competition_scale_id',
+                'competition_branch',
+                'competition_date',
+                'letter_of_acceptance',
+                'proposal',
+                'status',
+            ])
+            ->defaultSorts([
+                '-created_at',
+                'id',
+            ])
+            ->allowedSorts([
+                'id',
+                'team_id',
+                'competition_id',
+                'competition_team_type_id',
+                'competition_scale_id',
+                'competition_branch',
+                'competition_date',
+                'letter_of_acceptance',
+                'proposal',
+                'status',
+                'created_at',
+                'updated_at',
+            ])
+            ->paginate($request->query('per_page', 10));
+
+        return ResponseFormatter::collection('fund_applications', $fundApplications);
     }
 
     /**
@@ -27,7 +63,9 @@ class FundApplicationController extends Controller
      */
     public function store(StoreFundApplicationRequest $request)
     {
-        //
+        $fundApplication = FundApplication::create($request->validated());
+
+        return ResponseFormatter::singleton('fund_application', $fundApplication, 201);
     }
 
     /**
@@ -35,7 +73,7 @@ class FundApplicationController extends Controller
      */
     public function show(FundApplication $fundApplication)
     {
-        //
+        return ResponseFormatter::singleton('fund_application', $fundApplication);
     }
 
     /**
@@ -43,7 +81,9 @@ class FundApplicationController extends Controller
      */
     public function update(UpdateFundApplicationRequest $request, FundApplication $fundApplication)
     {
-        //
+        $fundApplication->update($request->validated());
+
+        return ResponseFormatter::singleton('fund_application', $fundApplication);
     }
 
     /**
@@ -51,6 +91,8 @@ class FundApplicationController extends Controller
      */
     public function destroy(FundApplication $fundApplication)
     {
-        //
+        $fundApplication->delete();
+
+        return ResponseFormatter::singleton('fund_application', $fundApplication);
     }
 }
