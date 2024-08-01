cfbe4e2f8a8f1e4e6b05421f2c782c0179147f75
Muhammad Naufal <me@falkia34.dev>
2024-08-01 13:51:05 +0700

    fix(controller): Mark controller base as abstract
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
     use AuthorizesRequests;
 }
