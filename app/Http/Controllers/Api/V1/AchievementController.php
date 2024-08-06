ddb04f36fb101a556f7244c1b2fc59639ff2918a
Muhammad Naufal <me@falkia34.dev>
2024-08-06 17:32:43 +0700

    feat(controller): Add `Achievement` CRUD
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Http/Controllers/Api/V1/AchievementController.php
+++ app/Http/Controllers/Api/V1/AchievementController.php
@@ -6,51 +6,99 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Achievement\StoreAchievementRequest;
 use App\Http\Requests\Achievement\UpdateAchievementRequest;
 use App\Models\Achievement;
+use App\Utils\ResponseFormatter;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class AchievementController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(Achievement::class, 'achievement');
+        // $this->authorizeResource(Achievement::class, 'achievement');
     }
 
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request): JsonResponse
     {
-        //
+        $achievements = QueryBuilder::for(Achievement::class)
+            ->allowedFilters([
+                'team_id',
+                'competition_id',
+                'competition_team_type_id',
+                'competition_scale_id',
+                'competition_time_range_id',
+                'competition_output_id',
+                'competition_rank_id',
+                'competition_branch',
+                'competition_date',
+                'description',
+                'image',
+                'status',
+            ])
+            ->defaultSorts([
+                '-created_at',
+                'id',
+            ])
+            ->allowedSorts([
+                'id',
+                'team_id',
+                'competition_id',
+                'competition_team_type_id',
+                'competition_scale_id',
+                'competition_time_range_id',
+                'competition_output_id',
+                'competition_rank_id',
+                'competition_branch',
+                'competition_date',
+                'description',
+                'image',
+                'status',
+                'created_at',
+                'updated_at',
+            ])
+            ->paginate($request->query('per_page', 10));
+
+        return ResponseFormatter::collection('achievements', $achievements);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreAchievementRequest $request)
+    public function store(StoreAchievementRequest $request): JsonResponse
     {
-        //
+        $achievement = Achievement::create($request->validated());
+
+        return ResponseFormatter::singleton('achievement', $achievement, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Achievement $achievement)
+    public function show(Achievement $achievement): JsonResponse
     {
-        //
+        return ResponseFormatter::singleton('achievement', $achievement);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateAchievementRequest $request, Achievement $achievement)
+    public function update(UpdateAchievementRequest $request, Achievement $achievement): JsonResponse
     {
-        //
+        $achievement->update($request->validated());
+
+        return ResponseFormatter::singleton('achievement', $achievement);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Achievement $achievement)
+    public function destroy(Achievement $achievement): JsonResponse
     {
-        //
+        $achievement->delete();
+
+        return ResponseFormatter::singleton('achievement', $achievement);
     }
 }
