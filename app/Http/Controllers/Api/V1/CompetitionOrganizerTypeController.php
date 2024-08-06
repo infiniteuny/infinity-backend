e8e085649af16dd6ee63a335db6f1cdf7db7a903
Muhammad Naufal <me@falkia34.dev>
2024-08-06 16:19:54 +0700

    feat(controller): Add `CompetitionOrganizerType` CRUD
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Http/Controllers/Api/V1/CompetitionOrganizerTypeController.php
+++ app/Http/Controllers/Api/V1/CompetitionOrganizerTypeController.php
@@ -6,51 +6,79 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CompetitionOrganizerType\StoreCompetitionOrganizerTypeRequest;
 use App\Http\Requests\CompetitionOrganizerType\UpdateCompetitionOrganizerTypeRequest;
 use App\Models\CompetitionOrganizerType;
+use App\Utils\ResponseFormatter;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class CompetitionOrganizerTypeController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(CompetitionOrganizerType::class, 'competitionOrganizerType');
+        // $this->authorizeResource(CompetitionOrganizerType::class, 'competition_organizer_type');
     }
 
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request): JsonResponse
     {
-        //
+        $competitionOrganizerTypes = QueryBuilder::for(CompetitionOrganizerType::class)
+            ->allowedFilters([
+                'name',
+                'weight',
+            ])
+            ->defaultSorts([
+                '-created_at',
+                'id',
+            ])
+            ->allowedSorts([
+                'id',
+                'name',
+                'weight',
+                'created_at',
+                'updated_at',
+            ])
+            ->paginate($request->query('per_page', 10));
+
+        return ResponseFormatter::collection('competition_organizer_types', $competitionOrganizerTypes);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCompetitionOrganizerTypeRequest $request)
+    public function store(StoreCompetitionOrganizerTypeRequest $request): JsonResponse
     {
-        //
+        $competitionOrganizerType = CompetitionOrganizerType::create($request->validated());
+
+        return ResponseFormatter::singleton('competition_organizer_type', $competitionOrganizerType, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(CompetitionOrganizerType $competitionOrganizerType)
+    public function show(CompetitionOrganizerType $competitionOrganizerType): JsonResponse
     {
-        //
+        return ResponseFormatter::singleton('competition_organizer_type', $competitionOrganizerType);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCompetitionOrganizerTypeRequest $request, CompetitionOrganizerType $competitionOrganizerType)
+    public function update(UpdateCompetitionOrganizerTypeRequest $request, CompetitionOrganizerType $competitionOrganizerType): JsonResponse
     {
-        //
+        $competitionOrganizerType->update($request->validated());
+
+        return ResponseFormatter::singleton('competition_organizer_type', $competitionOrganizerType);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(CompetitionOrganizerType $competitionOrganizerType)
+    public function destroy(CompetitionOrganizerType $competitionOrganizerType): JsonResponse
     {
-        //
+        $competitionOrganizerType->delete();
+
+        return ResponseFormatter::singleton('competition_organizer_type', $competitionOrganizerType);
     }
 }
