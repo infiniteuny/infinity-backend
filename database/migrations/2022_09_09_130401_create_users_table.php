1dd4898064d6d4bfee988b862e6d6d32a8f58fff
Muhammad Naufal <me@falkia34.dev>
2024-09-12 16:26:17 +0700

    feat(database): Add token table and sso_id attribute to users table
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- database/migrations/2022_09_09_130401_create_users_table.php
+++ database/migrations/2022_09_09_130401_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->string('sso_id')->unique();
             $table->string('name')->fulltext();
             $table->string('email_address')->unique();
             $table->string('phone_number')->unique();
@@ -26,6 +27,14 @@ return new class extends Migration
             $table->boolean('is_extraordinary')->default(false);
             $table->timestamps();
         });
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('external_id')->unique();
+            $table->foreignUuid('user_id')->constrained()->restrictOnUpdate()->cascadeOnDelete();
+            $table->timestamp('last_used_at');
+            $table->timestamp('created_at');
+            $table->timestamp('expires_at');
+        });
     }
 
     /**
@@ -34,5 +43,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('tokens');
     }
 };
