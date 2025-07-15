045c2c588180903729b89dd8c689781574c69206
Muhammad Naufal <me@falkia34.dev>
2025-07-15 23:28:11 +0700

    fix(model): Add group relation for Core Team and CG Admin
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Models/CoreTeam.php
+++ app/Models/CoreTeam.php
@@ -6,6 +6,7 @@ use DateTimeInterface;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class CoreTeam extends Model
@@ -29,6 +30,11 @@ class CoreTeam extends Model
     protected function serializeDate(DateTimeInterface $date): string
     {
         return $date->format(DATE_ATOM);
+    }
+
+    public function group(): BelongsTo
+    {
+        return $this->belongsTo(Group::class, 'group_id');
     }
 
     public function members(): BelongsToMany
