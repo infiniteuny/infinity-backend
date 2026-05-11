0fcd2d1825d1336854cbeb6d609f201626227176
Muhammad Naufal <me@falkia34.dev>
2026-05-11 08:18:35 +0700

    fix(provider): Bypass horizon auth gate to env directly
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
 
@@ -21,14 +20,14 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     }
 
     /**
-     * Register the Horizon gate.
-     *
-     * This gate determines who can access Horizon in non-local environments.
+     * Configure the Horizon authorization services.
      */
-    protected function gate(): void
+    protected function authorization(): void
     {
-        Gate::define('viewHorizon', function ($user = null) {
-            return config('horizon.enabled', false);
+        $this->gate();
+
+        Horizon::auth(function ($request) {
+            return config('horizon.enabled', false) || app()->environment('local');
         });
     }
 }
