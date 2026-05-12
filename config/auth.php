682fcf4ea15fe3751ad1140206dd2d2b1c2e229c
Muhammad Naufal <me@falkia34.dev>
2026-05-12 17:15:32 +0700

    fix(config): Update semi_public_guard to use 'api_headers' by default
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- config/auth.php
+++ config/auth.php
@@ -17,7 +17,7 @@ return [
 
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'api_headers'),
-        'semi_public_guard' => env('AUTH_SEMI_PUBLIC_GUARD', 'api_token'),
+        'semi_public_guard' => env('AUTH_SEMI_PUBLIC_GUARD', 'api_headers'),
     ],
 
     /*
