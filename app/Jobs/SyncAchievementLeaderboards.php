0f1d924f8bef0945c9f13ed82701da158b06dc82
Muhammad Naufal <me@falkia34.dev>
2026-05-17 12:17:03 +0700

    fix(job): Delete missing user achievement leaderboard from the same year
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Jobs/SyncAchievementLeaderboards.php
+++ app/Jobs/SyncAchievementLeaderboards.php
@@ -39,27 +39,27 @@ class SyncAchievementLeaderboards implements ShouldBeUnique, ShouldQueue
      */
     public function handle(): void
     {
-        $achievementLeaderboards = DB::table('achievements as a')
+        $achievementLeaderboards = DB::table('achievements AS a')
             ->select([
-                'users.id as user_id',
-                DB::raw('EXTRACT(YEAR FROM a.competition_start_date) as year'),
-                DB::raw('SUM(COALESCE(cot.weight,0) * COALESCE(ctt.weight,0) * COALESCE(cs.weight,0) * COALESCE(ctr.weight,0) * COALESCE(co.weight,0) * COALESCE(cr.weight,0)) as total_points'),
+                'users.id AS user_id',
+                DB::raw('EXTRACT(YEAR FROM a.competition_start_date) AS year'),
+                DB::raw('SUM(COALESCE(cot.weight,0) * COALESCE(ctt.weight,0) * COALESCE(cs.weight,0) * COALESCE(ctr.weight,0) * COALESCE(co.weight,0) * COALESCE(cr.weight,0)) AS total_points'),
             ])
-            ->join('competition_instances as ci', 'a.competition_instance_id', '=', 'ci.id')
-            ->join('teams as t', 'a.team_id', '=', 't.id')
-            ->join('team_members as tm', 't.id', '=', 'tm.team_id')
+            ->join('competition_instances AS ci', 'a.competition_instance_id', '=', 'ci.id')
+            ->join('teams AS t', 'a.team_id', '=', 't.id')
+            ->join('team_members AS tm', 't.id', '=', 'tm.team_id')
             ->join('users', 'tm.user_id', '=', 'users.id')
-            ->leftJoin('competition_organizer_types as cot', 'ci.organizer_type_id', '=', 'cot.id')
-            ->leftJoin('competition_team_types as ctt', 't.team_type_id', '=', 'ctt.id')
-            ->leftJoin('competition_scales as cs', 'a.competition_scale_id', '=', 'cs.id')
-            ->leftJoin('competition_time_ranges as ctr', 'a.competition_time_range_id', '=', 'ctr.id')
-            ->leftJoin('competition_outputs as co', 'a.competition_output_id', '=', 'co.id')
-            ->leftJoin('competition_ranks as cr', 'a.competition_rank_id', '=', 'cr.id')
+            ->leftJoin('competition_organizer_types AS cot', 'ci.organizer_type_id', '=', 'cot.id')
+            ->leftJoin('competition_team_types AS ctt', 't.team_type_id', '=', 'ctt.id')
+            ->leftJoin('competition_scales AS cs', 'a.competition_scale_id', '=', 'cs.id')
+            ->leftJoin('competition_time_ranges AS ctr', 'a.competition_time_range_id', '=', 'ctr.id')
+            ->leftJoin('competition_outputs AS co', 'a.competition_output_id', '=', 'co.id')
+            ->leftJoin('competition_ranks AS cr', 'a.competition_rank_id', '=', 'cr.id')
             ->groupBy('users.id', DB::raw('EXTRACT(YEAR FROM a.competition_start_date)'))
             ->whereYear('a.competition_start_date', $this->year)
             ->get();
 
-        AchievementLeaderboard::where('year', now()->year)->whereNotIn('user_id', $achievementLeaderboards->pluck('user_id'))
+        AchievementLeaderboard::where('year', $this->year)->whereNotIn('user_id', $achievementLeaderboards->pluck('user_id'))
             ->delete();
 
         foreach ($achievementLeaderboards as $row) {
